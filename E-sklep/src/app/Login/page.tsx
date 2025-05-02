9037a6e8048a5775ca086914c3faf2b905ca5f57
Stanislav Bazhan <stasbazhan06@gmail.com>
2025-05-02 15:13:11 +0200

    login komunikaty, ilosc koszyka, ikonka orders

--- E-sklep/src/app/Login/page.tsx
+++ E-sklep/src/app/Login/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import "../../styles/login.css";
 import Link from 'next/link';
 
@@ -22,84 +22,119 @@ export default function Page() {
         };
     }, []);
     return (
-        <div className="container">
-            <div className="form-box login">
-                <form action="#">
-                    <h1>Login</h1>
-                    <div className="input-box">
-                        <input type="text" placeholder="Username" required />
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 bxs-user">
-                            <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
-                        </svg>
-                    </div>
-
-                    <div className="input-box">
-                        <input type="password" placeholder="Password" required />
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 bxs-lock-alt">
-                            <path fillRule="evenodd" d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3c0-2.9-2.35-5.25-5.25-5.25Zm3.75 8.25v-3a3.75 3.75 0 1 0-7.5 0v3h7.5Z" clipRule="evenodd" />
-                        </svg>
-                    </div>
-
-                    <div className="forgot-link">
-                        <a href="#">Forgot Password?</a>
-                    </div>
-                    <button type="submit" className="btn">Login</button>
-                    <p>or login with social platforms</p>
-                    <div className="social-icons">
-                        <a href="#"><img src="/google-logo-24.png" alt=""/></a>
-                        <a href="#"><img src="github-logo-24.png" alt=""/></a>
-                    </div>
-                </form>
-            </div>
-
-            <div className="form-box register">
-                <form action="#">
-                    <h1>Registration</h1>
-                    <div className="input-box">
-                        <input type="text" placeholder="Username" required />
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                            <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
-                        </svg>
-                    </div>
-
-                    <div className="input-box">
-                        <input type="email" placeholder="Email" required />
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                            <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
-                            <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
-                        </svg>
-                    </div>
-
-                    <div className="input-box">
-                        <input type="password" placeholder="Password" required />
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                            <path fillRule="evenodd" d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3c0-2.9-2.35-5.25-5.25-5.25Zm3.75 8.25v-3a3.75 3.75 0 1 0-7.5 0v3h7.5Z" clipRule="evenodd" />
-                        </svg>
-                    </div>
-
-                    <button type="submit" className="btn">Register</button>
-                    <p>or register with social platforms</p>
-                    <div className="social-icons">
-                        <a href="#"><img src="/google-logo-24.png" alt=""/></a>
-                        <a href="#"><img src="github-logo-24.png" alt=""/></a>
-                    </div>
-                </form>
-            </div>
-
-            <div className="toggle-box">
-                <div className="toggle-panel toggle-left">
-                    <h1>Hello, Welcome!</h1>
-                    <p>Don't have an account?</p>
-                    <button className="btn register-btn">Register</button>
-                </div>
-
-                <div className="toggle-panel toggle-right">
-                    <h1>Welcome Back!</h1>
-                    <p>Already have an account?</p>
-                    <button className="btn login-btn">Login</button>
+        <>
+            <div className="login-communications">
+                {/*Zmieniania klasa z succes na error jezeli jest error */}
+                <div className="communications-content success">
+                    <h3 className="title">Success</h3>
+                    <p className="sub-title">Login successful</p>
+                    {/*Udalo sie*/}
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                         stroke="currentColor" className="size-6 success-icon">
+                        <path strokeLinecap="round" strokeLinejoin="round"
+                              d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                    </svg>
+                    {/*Nie udalo sie*/}
+                    {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 error-icon">*/}
+                    {/*   <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />*/}
+                    {/* </svg> */}
                 </div>
             </div>
+            <div className="login-reg-container">
+                <div className="container">
+                    <div className="form-box login">
+                        <form action="#">
+                            <h1>Login</h1>
+                            <div className="input-box">
+                                <input type="text" placeholder="Username" required/>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                     className="size-6 bxs-user">
+                                    <path fillRule="evenodd"
+                                          d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                                          clipRule="evenodd"/>
+                                </svg>
+                            </div>
 
-        </div>
+                            <div className="input-box">
+                                <input type="password" placeholder="Password" required/>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                     className="size-6 bxs-lock-alt">
+                                    <path fillRule="evenodd"
+                                          d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3c0-2.9-2.35-5.25-5.25-5.25Zm3.75 8.25v-3a3.75 3.75 0 1 0-7.5 0v3h7.5Z"
+                                          clipRule="evenodd"/>
+                                </svg>
+                            </div>
+
+                            <div className="forgot-link">
+                                <a href="#">Forgot Password?</a>
+                            </div>
+                            <button type="submit" className="btn">Login</button>
+                            <p>or login with social platforms</p>
+                            <div className="social-icons">
+                                <a href="#"><img src="/google-logo-24.png" alt=""/></a>
+                                <a href="#"><img src="github-logo-24.png" alt=""/></a>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div className="form-box register">
+                        <form action="#">
+                            <h1>Registration</h1>
+                            <div className="input-box">
+                                <input type="text" placeholder="Username" required/>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                     className="size-6">
+                                    <path fillRule="evenodd"
+                                          d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                                          clipRule="evenodd"/>
+                                </svg>
+                            </div>
+
+                            <div className="input-box">
+                                <input type="email" placeholder="Email" required/>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                     className="size-6">
+                                    <path
+                                        d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z"/>
+                                    <path
+                                        d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z"/>
+                                </svg>
+                            </div>
+
+                            <div className="input-box">
+                                <input type="password" placeholder="Password" required/>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                     className="size-6">
+                                    <path fillRule="evenodd"
+                                          d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3c0-2.9-2.35-5.25-5.25-5.25Zm3.75 8.25v-3a3.75 3.75 0 1 0-7.5 0v3h7.5Z"
+                                          clipRule="evenodd"/>
+                                </svg>
+                            </div>
+
+                            <button type="submit" className="btn">Register</button>
+                            <p>or register with social platforms</p>
+                            <div className="social-icons">
+                                <a href="#"><img src="/google-logo-24.png" alt=""/></a>
+                                <a href="#"><img src="github-logo-24.png" alt=""/></a>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div className="toggle-box">
+                        <div className="toggle-panel toggle-left">
+                            <h1>Hello, Welcome!</h1>
+                            <p>Don't have an account?</p>
+                            <button className="btn register-btn">Register</button>
+                        </div>
+
+                        <div className="toggle-panel toggle-right">
+                            <h1>Welcome Back!</h1>
+                            <p>Already have an account?</p>
+                            <button className="btn login-btn">Login</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
--- E-sklep/src/app/_components/Header.tsx
+++ E-sklep/src/app/_components/Header.tsx
@@ -74,6 +74,19 @@ export default function Header() {
                             </div>
 
                         </div>
+                        <div className="cart-content">
+                            <Link href="/Koszyk">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                     strokeWidth={1.5} stroke="currentColor" className="size-8 cart">
+                                    <path strokeLinecap="round" strokeLinejoin="round"
+                                          d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"/>
+                                </svg>
+                            </Link>
+                            <div className="cart-number">
+                                {/* <!-- liczba zmieniajaca sie wedlug liczby dodanych przedmiotow do likow(Jezeli nie ma zadnych przedmiotow to like number jest wylaczane (display: block;)) --> */}
+                                2
+                            </div>
+                        </div>
                         <div className="user">
                             <div className="user-pfp">
                                 <Link href="/Login">
@@ -91,13 +104,10 @@ export default function Header() {
                                     <path strokeLinecap="round" strokeLinejoin="round"
                                           d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"/>
                                 </svg>
-                                <Link href="/Koszyk">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         strokeWidth={1.5} stroke="currentColor" className="size-6 cart">
-                                        <path strokeLinecap="round" strokeLinejoin="round"
-                                              d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"/>
-                                    </svg>
-                                </Link>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 orders">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
+                                </svg>
+
 
                             </div>
                             <UserToggle/>
